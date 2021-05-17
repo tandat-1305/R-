96975f6e12212a145af51d7768340c94ae3b61d3
TanDat_Eg <72129432+tandat-1305@users.noreply.github.com>
2021-05-17 22:15:03 +0700

    Update solution.R

--- csdl-solution/solution.R
+++ csdl-solution/solution.R
@@ -23,6 +23,28 @@ xlab = "Days since 1 January 1990",
 ylab = "Temperature",
 main = Station.i)
 dev.off
+  
+  
 # Exercise 2
 Owls <-read.table(file = "Owls.txt", header = TRUE, dec = ".")
-names(Owls) str(Owls)
+names(Owls) 
+str(Owls)
+ifelse(Owls FoodTreatment == "Satiaded", Owls$NestNight <- paste(Owls$Nest, "1", sep="_"),
+Owls$NestNight <- paste(Owls$Nest, "2".sep="_"))
+head(Owls)
+#You can also do this with: Owls$NestNight <- paste(Owls$Nest, Owls FoodTreatment,sep="_")
+AllNestsNights <- unique(Owls$NestNight)
+N<- length(AllNestsNights) 
+for (i in 1:N) {
+  NestNight.i <-as.character(AllNestsNights[i]) 
+  print(NestNight.i) 
+  Owlsi <- Owls[Owls$NestNight == NestNight.i ,]
+  YourFileName <- paste(NestNight.i, ".jpg", sep="") 
+  #jpeg(file = YourFileName) 
+  plot(x = Owlsis Arrival Time , y=OwlsiSNegPerChick, 
+       xlab = "Arrival Time".
+      ylab = "Sibling negotiation", 
+       main = NestNight.i) 
+  #dev.off
+  }
+

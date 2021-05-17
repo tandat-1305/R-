dd681381239459a2f1676e3bf9aa0ece2b39cdf9
TanDat_Eg <72129432+tandat-1305@users.noreply.github.com>
2021-05-17 21:09:54 +0700

    Create solution.R

---
+++ csdl-solution/solution.R
@@ -0,0 +1,28 @@
+#Solutions Chapter 6. Function...
+# Exercise 1 #This code is computer specific.
+# Set working direction (folder)
+setwd(".................")
+Convert temperature.xls => temperature.txt
+#Declare a frame variable to store temperature.txt data. In my case, the separate between two columns is *** signal
+TP <- read.table(file = "Temperature.txt", header = TRUE, dec = ".") 
+#You should check all variables (column), it should available for analysing...
+names(TP)
+str(TP)
+#Our function:
+#Check how many station (tram khi tuong) in our dataset
+AllStations <- unique(TP$Station)
+N<- length(AllStations)
+
+for (i in 1:N) {
+Station.i<-as.character(AllStations[i])
+print(Station.i) TPI <- TP[TP$Station Station..]
+YourFileName <- paste(Station.i, ".jpg", sep="")
+jpeg(file = YourFileName) 
+plot(x = TPi$dDay2 , y = TPi$Temperature,
+xlab = "Days since 1 January 1990",
+ylab = "Temperature",
+main = Station.i)
+dev.off
+# Exercise 2
+Owls <-read.table(file = "Owls.txt", header = TRUE, dec = ".")
+names(Owls) str(Owls)

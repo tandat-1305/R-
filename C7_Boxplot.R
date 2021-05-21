77ca714ccd7e6ae9833db33d193c3c75163ba5a9
TanDat_Eg <72129432+tandat-1305@users.noreply.github.com>
2021-05-21 19:33:30 +0700

    Create C7_Boxplot.R

---
+++ C7_Boxplot.R
@@ -0,0 +1,46 @@
+setwd("D:\\Download_D\\Chapter 7")
+Owls <- read.table('Owls.txt', sep = '\t', header = TRUE)
+
+names(Owls)
+Owls
+str(Owls)
+
+boxplot(Owls$NegPerChick)
+par(mfrow=c(2,2), mar = c(3,3,2,1))
+boxplot(NegPerChick ~ SexParent, data = Owls)
+boxplot(NegPerChick ~ FoodTreatment, data = Owls)
+boxplot(NegPerChick ~ SexParent* FoodTreatment,
+        names = c(1,2,3,4),
+        data = Owls)
+boxplot(NegPerChick ~ Nest, data = Owls)
+
+
+par(mar = c(2,2,3,3))
+boxplot(NegPerChick ~ Nest,
+        axes=FALSE,
+        ylim=c(-3,8.5),
+        data = Owls)
+unique(Owls$Nest)
+axis(2, at=c(0,2,4,6,8))
+text(x=1:27, y=-2, labels = unique(Owls$Nest),
+     cex=1, srt=65)
+     
+     
+# 7.3 Boxplot sử dụng cho dữ liệu sinh vật biển RIKZ2.txt
+# vẽ viểu đồ boxplot giữa
+# Richness ~ Beach
+
+
+RIKZ = read.table('RIKZ2.txt', header =TRUE)
+RIKZ
+
+
+boxplot(Richness ~ Beach, data = RIKZ,
+        col ='grey',
+        xlab="Beach",
+        ylab = 'Richness')
+
+
+Veg = read.table('Vegetation2.txt', header = TRUE)
+boxplot(R~Transect, data = Veg,
+        ylab = 'Richness')

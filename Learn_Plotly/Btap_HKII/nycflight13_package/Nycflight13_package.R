8653de9cead2e5bd8a60665e746de09910333c4d
TanDat_Ez <72129432+tandat-1305@users.noreply.github.com>
2021-09-21 09:52:17 +0700

    Add files via upload

---
+++ Learn_Plotly/Btap_HKII/nycflight13_package/Nycflight13_package.R
@@ -0,0 +1,74 @@
+library(nycflights13)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(plotly)
+
+str(airlines)
+data("airlines")
+str(flights)
+data(flights)
+#==============================================================================
+fontStyle = list(
+  family = "DM Sans", #Kieu chu
+  size = 20,          #Kich thuoc chut
+  color = 'red'    #Mau chu
+)
+#==============================================================================
+#CAU 1
+data <- flights %>%
+  dplyr::select(arr_delay, dep_delay,carrier) %>%  #
+  filter(carrier == 'AS')                          #filter: bo loc 
+data
+
+data %>%
+  plot_ly() %>%
+  add_markers(x ~dep_delay, y = ~arr_delay,
+              marker = list(size = 8,
+                            color = 'rgba(157, 237, 59, 1)',
+                            line = list(color = 'rgba(33, 197, 211, 1)',
+                                        width = 2))) %>%
+  layout(title = 'Chuyen bay cua hãng ALASKA AIRLINE',font = fontStyle)
+#==============================================================================
+#CAU 2
+str(weather)
+data(weather)
+data2 <- weather %>%
+  dplyr::select(origin, temp, time_hour, month, day ) %>%
+  filter(origin == 'EWR', month == 1, day < 15)
+
+data2 %>%
+  plot_ly(x = ~time_hour, y = ~temp, type = 'scatter', mode = 'lines',color = I('rgba(157, 237, 59, 1)')) %>%
+  layout(title = 'Nhiet do các ngày trong tháng 1',font = fontStyle)
+#==============================================================================
+#CAU 3
+t <- count(weather,vars=temp)
+names(t)[1] <- paste("temp")
+names(t)[2] <- paste("count")
+data3 <- t
+data3 %>%
+  plot_ly(x = ~temp, type = 'histogram')
+
+ #==============================================================================
+#CAU 4 
+data4 <- weather %>%
+  dplyr::select(temp, month)
+
+data4%>%
+  plot_ly(x = ~month, y = ~temp, type = 'box',color = I('rgba(33, 197, 211, 1)')) %>%
+  layout(title = 'Su phân bo cua nhiet do theo các tháng',font=fontStyle)
+#==============================================================================
+#CAU 5
+sum_carrier = aggregate(flights$flight, by = list(carrier = flights$carrier), FUN =sum)
+names(sum_carrier)[2]<-paste("count")
+
+data5 <- sum_carrier
+data5 %>%
+  plot_ly(x = ~carrier, y = ~count, type = 'bar') %>%
+  layout(title = 'So chuyen bay cua các hang hang khong',font = fontStyle)
+
+
+
+
+
+

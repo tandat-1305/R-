ec571280bd487a569eeef755bcd17749a4ae59be
TanDat_Ez <72129432+tandat-1305@users.noreply.github.com>
2021-11-14 18:37:35 +0700

    Note
    fontStyle
    Barchart
    Nghiên cứu về tình hình học tập của sinh viên trong 3 năm.

---
+++ Learn_Plotly/PT_solieu_dinhluong.R
@@ -0,0 +1,47 @@
+library(plotly)
+
+#Tao font chu
+fontStyle = list(
+  family = "DM Sans", #Kieu chu
+  size = 20,          #Kich thuoc chut
+  color = "grey"     #Mau chu
+)
+
+#Nhap du lieu
+x <- c("2016-2017", "2017-2018", "2018-2019")
+y <- c("45", "32", "40")
+y2 <- c("15", "18", "12")
+y3 <- c("18", "21", "11")
+y4 <- c(0,0,0)
+
+#Tao bang
+data <- data.frame(x, y, y2, y3, y4)
+names(data)[1]<-paste("Year")
+names(data)[2]<-paste("Khá gi???i")
+names(data)[3]<-paste("H???c l???i")
+names(data)[4]<-paste("H???c b???ng")
+
+#Ve bieu do cot 
+
+fig <- data %>% plot_ly()
+fig <- fig %>% add_trace(x = ~x, y = ~y, type = 'bar', name = "Khá gi???i",
+                         text = y, textposition = 'auto',
+                         marker = list(color = '#38EB00',
+                                       line = list(color = 'rgb(8,48,107)', width = 1.5)))
+fig <- fig %>% add_trace(x = ~x, y = ~y2, type = 'bar', name = "H???c l???i",
+                         text = y2, textposition = 'auto',
+                         marker = list(color = '#E85D15',
+                                       line = list(color = 'rgb(8,48,107)', width = 1.5)))
+fig <- fig %>% add_trace(x = ~x, y = ~y3, type = 'bar', name = "H???c b???ng",
+                         text = y3, textposition = 'auto',
+                         marker = list(color = '#2C2CE8',
+                                       line = list(color = 'rgb(8,48,107)', width = 1.5)))
+fig <- fig %>% add_trace(x = ~x, y = ~y4, type = 'bar', name = FALSE,
+                         text = y4, textposition = 'auto',
+                         marker = list(color = 'rgb(58,200,225)',
+                                       line = list(color = 'rgb(8,48,107)', width = 1.5)))
+fig <- fig %>%
+  layout(xaxis = list(title = "Year"),
+         yaxis = list(title ="Percentage (%)"),
+         font = fontStyle)
+fig
